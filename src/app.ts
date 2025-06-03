7d18df82b464922f96209bfe85bb717e253b39ca
bahari <haribahari71@gmail.com>
2025-06-04 02:18:13 +0700

    update git ignore and add github yml

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import helloRouter from './routes/hello.route';
 import loggerMiddleware from './middlewares/logger.middleware';
 import morgan from 'morgan';
 import winston from 'winston';
-import userRoute from './routes/user.route'
-import authRoute from './routes/auth.route'
-import projectRoute from './routes/project.route'
-import categoryRoute from './routes/category.routes'
+import userRoute from './routes/user.route';
+import authRoute from './routes/auth.route';
+import projectRoute from './routes/project.route';
+import categoryRoute from './routes/category.routes';
 
 const app: Application = express();
 
@@ -35,15 +35,23 @@ app.use(
 // Middleware bawaan Express untuk parsing JSON body
 app.use(express.json());
 
-// Pasang middleware custom (contohnya logger sederhana)
+// Pasang middleware custom
 app.use(loggerMiddleware);
 
 // Daftarkan route
 app.use('/api/hello', helloRouter);
-app.use('/api/users', userRoute)
-app.use('/api/auth', authRoute)
-app.use('/api/projects', projectRoute)
-app.use('/api/categories', categoryRoute)
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/projects', projectRoute);
+app.use('/api/categories', categoryRoute);
+
+// Endpoint utama (root) dengan pesan sambutan dan link dokumentasi
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    message: 'Welcome to the GWT CMS Backend API. Visit /api/* endpoints to access resources.',
+    documentation: 'https://xus367y7ar.apidog.io/'
+  });
+});
 
 // Route fallback jika tidak ditemukan
 app.use((req: Request, res: Response) => {
