3a89d01b7ed57075b0916f0642fa9029bd14f02b
bahari <haribahari71@gmail.com>
2025-06-14 05:09:59 +0700

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,10 +8,6 @@ module.exports = {
       watch: false,                  // Nonaktifkan watch (jangan restart otomatis saat file berubah)
       autorestart: true,             // Otomatis restart jika crash
       max_restarts: 5,               // Maksimal restart sebelum PM2 menyerah
-      env: {
-        NODE_ENV: 'development',
-        PORT: 3000
-      },
       env_production: {
         NODE_ENV: 'production',
         PORT: 3012
